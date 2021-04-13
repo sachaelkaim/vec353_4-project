82de40137f56d2577821b83c83c6bdb13867e5af
Sacha Elkaim <sachaelkaim87@gmail.com>
2021-04-13 17:57:55 -0400

    update

--- front-end/client/src/App.js
+++ front-end/client/src/App.js
@@ -2,8 +2,6 @@ import "./App.css";
 import React from "react";
 import Navigation from "./components/Navigation.js";
 import Home from "./components/Home.js";
-import Home1 from "./components/Home1.js";
-import Home2 from "./components/Home2.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -13,8 +11,6 @@ function App() {
         <Navigation />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/1" component={Home1} />
-          <Route exact path="/2" component={Home2} />
           <Route path="*" component={Home} />
         </Switch>
       </Router>
--- front-end/client/src/components/Navigation.js
+++ front-end/client/src/components/Navigation.js
@@ -7,9 +7,6 @@ const Navigation = () => {
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="/">VEC353_4</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="/">1-6</Nav.Link>
-          <Nav.Link href="/1">7-13</Nav.Link>
-          <Nav.Link href="/2">14-17</Nav.Link>
         </Nav>
       </Navbar>
     </div>
